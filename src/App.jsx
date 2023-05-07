d5a5569c5848b02d21db4e5101b64722414a4f9b
rinshadcp <rinshadcp7337@gmail.com>
2023-05-07 11:36:33 +0530

    add all functionality for todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,64 @@
+import { useState } from 'react'
 import './style.css'
 export default function App(){
+const [newItem,setNewItem] =  useState('')
+const [todos,setTodos] =useState([])
+
+function handleSubmit(e){
+
+ e.preventDefault()
+setTodos((currentTodos)=>{
+  return [
+    ...currentTodos,{
+    id:crypto.randomUUID(),title:newItem,completed:false
+  }]
+}
+)
+setNewItem("")
+}
+function toggleTodo(id,completed){
+  setTodos(currentTodos=>{
+    return currentTodos.map(todo =>{
+    if(todo.id===id){
+      return {...todo,completed}
+    }
+return todo
+    })
+
+  })
+}
+function deleteTodo(id){
+setTodos(currentTodos=>{
+  return currentTodos.filter(todo=>todo.id !==id)
+})
+}
  return (
   <>
- <form  className="new-item-form">
+ <form onSubmit={handleSubmit} className="new-item-form">
   <div className="form-row">
     <label htmlFor="item">New Item</label>
-    <input type="text" id="item"/>
+    <input type="text" value={newItem} onChange={e=>setNewItem(e.target.value)} id="item"/>
   </div>
     <button className='btn'> Add</button>
  </form>
  <h1 className='header'>Todo List</h1>
- <ul className='list'><li>
- <label >
-    <input type="checkbox" />
-    item1
-  </label>
-  
-  <button className="btn btn-danger">Delete</button> 
-  </li>
-  <li>
+ <ul className='list'>
+  {todos.map(todo=>{
+    return ( 
 
-  <label >
-    <input type="checkbox" />
-    item2
+    
+  <li key={todo.id}>
+ <label >
+    <input type="checkbox" checked={todo.completed} 
+    onChange={e=>toggleTodo(todo.id,e.target.checked)}/>
+    {todo.title}
   </label>
   
-  <button className="btn btn-danger">Delete</button>
-  </li></ul>
+  <button onClick={()=>deleteTodo} className="btn btn-danger">Delete</button> 
+  </li>
+  )
+  })}
+ </ul>
   </>
 
  )
